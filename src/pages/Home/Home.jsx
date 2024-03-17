2d9229fd2f01b3030b2c0f73022aef788224e6f7
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-03-17 16:37:32 +0700

    Fix the tailwind config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 import Song from "../../components/common/Song";
 import RecentlyPlayed from "../../components/HomePage/RecentlyPlayed";
-
 export default function Home() {
     return (
-        <div>
-            <RecentlyPlayed />
-        </div>
-    );
+      <>
+<div className="home-container ">
+  <div className="left-space">
+    {/* first blank space  */}
+    space 1
+  </div>
+  <div className="home-song-items">
+    <RecentlyPlayed/>
+  </div>
+  <div className="personal-recommendation">
+    //personal recommendations
+  </div>
+  <div className="right-space">
+    {/* last space */}
+    space2
+  </div>
+</div>    
+</>
+);
 }
